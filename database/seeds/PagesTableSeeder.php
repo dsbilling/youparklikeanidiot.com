9a400067251940bfb75c70d8554f364cfac2a5b1
Daniel Billing <daniel@infihex.com>
2018-04-08 21:59:37 +0200

    Needed more pages to test with

--- database/seeds/PagesTableSeeder.php
+++ database/seeds/PagesTableSeeder.php
@@ -7,26 +7,37 @@ class PagesTableSeeder extends Seeder  {
 
 	public function run() {
 
-		/*Page::create([
-			'title' 		=> 'Regler',
-			'slug' 			=> 'rules',
+		Page::create([
+			'title' 		=> 'Om',
+			'slug' 			=> 'om',
 			'content'		=> '
-			<ul>
-				<li>Alle deltagere må kunne identifisere seg samt vise bilett til alle tider. Vi ber derfor alle om å ta med identifikasjon med bilde. Bankkort, pass, førerkort osv.</li>
-				<li>Snusing, røyking og besittelse av alkohol eller narkotiske stoffer er ikke tillat innenfor området Downlink arrangers. Skal du røyke eller snuse, er det designerte plasser for dette.</li>
-			</ul>
-			<br>
-			<h5>Annet</h5>
-			<ul>
-				<li>Alle forsøk på å komme seg inn i gymsalen utenom designert inngang er strengt forbudt. Våre vakter patruljerer både gymsalen og området rundt 24/7. Om noen skulle komme seg inn uten deltagerarmbånd, vil de bli bortvist fra området.</li>
-				<li>Vi tar ikke ansvar for tyveri, men for sjansen for dette skal bli minimal ber vi deg om å sikre dine verdifulle gjenstander. Få sidemannen til å passe på utstyret ditt mens du sover eller er borte fra plassen din. Skulle likevel et tyveri oppstå, ber vi deg anmeldet forholdet ved å kontakte politiet direkte eller via <a href="http://politi.no">politi.no</a></li>
-				<li>Ved en eventuell evakuering hvor alle må forlate bygget, ber vi deg gå til nærmeste nødutgang. Ikke ta med datamaskinen din. Hjelp andre med å komme seg ut, og følg alle instruksjoner fra crewet</li>
-				<li>Downlink reserveres seg retten til å bortvise deltagere som bestrider vårt reglement eller norsk lov, uten refusjon av billett, reise eller andre utlegg. Om nødvendig vil vi også anmelde forholdet til Politiet.</li>
-			</ul>
+			<p>Denne siden kan kanskje virke litt "dust", men det er ikke hensikten.</p>
+			<h3>Hvorfor bruker dere ordet "idiot"?</h3>
+			<p>Det kan kanskje virke nedverdigende for noen folk imens andre tar ikke dette ordet seriøst. Det finnes mange beskrivende ord, men dette er det var det åpenbare ordet å bruke når dette prosjektet startet. Mange mener at dette ordet er det best beskrivende ordet for dem som parkerer slik som de parkerer.</p>
+			',
+			'showinmenu'	=> 1,
+			'author_id'		=> 1,
+		]);
+
+		Page::create([
+			'title' 		=> 'Terms of Service',
+			'slug' 			=> 'tos',
+			'content'		=> '
+			<p>Nothing here yet.</p>
 			',
 			'showinmenu'	=> 0,
 			'author_id'		=> 1,
-		]);*/
+		]);
+
+		Page::create([
+			'title' 		=> 'Privacy Policy',
+			'slug' 			=> 'privacy',
+			'content'		=> '
+			<p>Nothing here yet.</p>
+			',
+			'showinmenu'	=> 0,
+			'author_id'		=> 1,
+		]);
 		
 
 	}
