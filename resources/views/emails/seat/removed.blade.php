6a3c41da6c220b5bebba30b109f651d6ba653815
Daniel Billing <daniel@infihex.com>
2018-04-15 20:14:34 +0200

    Removed uneeded views

--- resources/views/emails/seat/removed.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('emails.layouts.main')
-@section('subject', 'Reservasjon fjernet!')
-@section('content') 
-
-Your reservation for the {{ $seatname }} seat has been removed, since you have not paid for it within 48 hours of reservation time.<br>
-
-@stop
--- resources/views/emails/seat/removedsoon.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('emails.layouts.main')
-@section('subject', 'Din reservasjon vil bli fjernet snart')
-@section('content') 
-
-Your reservation for the {{ $seatname }} seat will be removed in 24 hours, if you do not pay for the seat.<br>
-
-@stop
