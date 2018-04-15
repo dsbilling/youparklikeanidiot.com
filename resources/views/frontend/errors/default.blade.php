170884d39684d9b952ee1f9cce0406a184345205
Daniel Billing <daniel@infihex.com>
2018-04-15 20:17:12 +0200

    Updated default error page

--- resources/views/frontend/errors/default.blade.php
+++ resources/views/frontend/errors/default.blade.php
@@ -1,13 +1,7 @@
-@extends('layouts.errors')
+@extends('layouts.home')
 @section('title', 'Unknown Error')
-   
+
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-6 col-md-offset-3 text-center">
-			<h1 class="error-header">{{ $code }}</h1>
-			<h3 class="error-lead text-muted">Unexpected code malfunction was encountered.</h3>
-		</div>
-	</div>
-</div> 
+	<h1 class="display-4">{{ $code }} Unknown Error</h1>
+	<p class="lead">Unexpected code malfunction was encountered.</p>
 @stop
