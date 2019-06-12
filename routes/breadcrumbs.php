02020977e9a7d4e7c5c673469a4e4b568a3f18d4
Daniel Billing <daniel@infihex.com>
2019-06-12 22:20:45 +0200

    Fixed breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,5 +1,10 @@
 <?php
 
+// Home
+Breadcrumbs::for('home', function ($trail) {
+     $trail->push('Home', route('home'));
+});
+
 Breadcrumbs::for('login', function ($trail) {
     $trail->parent('home');
     $trail->push('Logg Inn', route('login'));
@@ -27,55 +32,29 @@ Breadcrumbs::macro('pageTitle', function () {
 });
 
 Breadcrumbs::macro('resource', function ($name, $title) {
-    // Home > Blog
-    Breadcrumbs::for("$name.index", function ($trail) use ($name, $title) {
+    // Home > Name
+    Breadcrumbs::for($name.".index", function ($trail) use ($name, $title) {
         $trail->parent('home');
-        $trail->push($title, route("$name.index"));
+        $trail->push($title, route($name.".index"));
     });
 
-    // Home > Blog > New
-    Breadcrumbs::for("$name.create", function ($trail) use ($name) {
-        $trail->parent("$name.index");
-        $trail->push('Opprett', route("$name.create"));
+    // Home > Name > New
+    Breadcrumbs::for($name.".create", function ($trail) use ($name) {
+        $trail->parent($name.".index");
+        $trail->push('Opprett', route($name.".create"));
     });
 
-    // Home > Blog > Post 123
-    Breadcrumbs::for("$name.show", function ($trail, $model) use ($name) {
-        $trail->parent("$name.index");
-        $trail->push($model->id, route("$name.show", $model));
+    // Home > Name > Post 123
+    Breadcrumbs::for($name.".show", function ($trail, $id) use ($name) {
+        $trail->parent($name.".index");
+        $trail->push($id, route($name.".show", $id));
     });
 
-    // Home > Blog > Post 123 > Edit
-    Breadcrumbs::for("$name.edit", function ($trail, $model) use ($name) {
-        $trail->parent("$name.show", $model);
-        $trail->push('Rediger', route("$name.edit", $model));
+    // Home > Name > Post 123 > Edit
+    Breadcrumbs::for($name.".edit", function ($trail, $id) use ($name) {
+        $trail->parent($name.".show", $id);
+        $trail->push('Rediger', route($name."edit", $id));
     });
 });
 
-Breadcrumbs::macro('crud', function (string $module, string $parent, string $prefix) {
-    // Module Name
-    Breadcrumbs::register($prefix . '.index', function ($breadcrumbs) use ($module, $parent, $prefix) {
-        $breadcrumbs->parent($parent);
-        $breadcrumbs->push($module, route($prefix . '.index'));
-    });
-    // Module Name > Add
-    Breadcrumbs::register($prefix . '.create', function ($breadcrumbs) use ($prefix) {
-        $breadcrumbs->parent($prefix . '.index');
-        $breadcrumbs->push(__('Add'), route($prefix . '.create'));
-    });
-    // Module Name > Details
-    Breadcrumbs::register($prefix . '.show', function ($breadcrumbs, $id) use ($prefix) {
-        $breadcrumbs->parent($prefix . '.index');
-        $breadcrumbs->push(__('Details'), route($prefix . '.show', $id));
-    });
-    // Module Name > Edit
-    Breadcrumbs::register($prefix . '.edit', function ($breadcrumbs, $id) use ($prefix) {
-        $breadcrumbs->parent($prefix . '.show', $id);
-        $breadcrumbs->push(__('Update'), route($prefix . '.edit', $id));
-    });
-});
-
-Breadcrumbs::resource('blog', 'Blog');
-Breadcrumbs::for('c', function () {
-    Breadcrumbs::crud('Condo', 'main', 'c');
-});
+Breadcrumbs::resource('parking', 'Parkeringer');
