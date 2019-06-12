008a713fe2354e90ff96b20a477523c669fbadf4
Daniel Billing <daniel@infihex.com>
2019-06-12 12:05:04 +0200

    Updated application name

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace DPSEI\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use DPSEI\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace DPSEI\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
