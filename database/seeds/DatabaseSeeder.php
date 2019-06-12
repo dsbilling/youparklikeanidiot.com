83de52ed0d6e8cc28cdf4b9fe9a2e33adaa84fd1
Daniel Billing <daniel@infihex.com>
2019-06-12 22:22:03 +0200

    Added user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(LicensePlateTableSeeder::class);
         $this->call(SubmissionTableSeeder::class);
         $this->call(ImageTableSeeder::class);
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+        	'uuid' => Str::uuid(4),
+            'name' => Str::random(10),
+            'username' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
