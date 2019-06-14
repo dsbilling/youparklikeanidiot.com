c7a658ff50d9b13553beaa576c5f37b4a4d3d76a
Daniel Billing <daniel@infihex.com>
2019-06-14 10:05:26 +0200

    Added all types to seeder

--- database/seeds/TypeTableSeeder.php
+++ database/seeds/TypeTableSeeder.php
@@ -13,11 +13,78 @@ class TypeTableSeeder extends Seeder
     {
         DB::table('types')->insert([
             'uuid' => Str::uuid(4),
+            'title' => 'To plasser, en bil',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Parkering/all stans forbudt',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'For langt unna innkjørselen',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'For nær/i innkjørsel',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Over de malte linjene',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Dobbeltparkering',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Parkert på handicap plass uten merke',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'I sykkelfelt',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'På fortauet',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Reservert ladestasjon for EL-kjøretøy',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Parkert på ladestasjon uten å lade',
+        ]);
+
+        DB::table('types')->insert([
+            'uuid' => Str::uuid(4),
+            'title' => 'Annet, se kommentar',
         ]);
 
         DB::table('submission_type')->insert([
             'type_id' => 1,
             'submission_id' => 1,
         ]);
+
+        DB::table('submission_type')->insert([
+            'type_id' => 2,
+            'submission_id' => 1,
+        ]);
+
+        DB::table('submission_type')->insert([
+            'type_id' => 3,
+            'submission_id' => 1,
+        ]);
+
     }
 }
