0848bbdffda284b2f5d0ff7826f443d89d5051e1
Daniel Billing <daniel@infihex.com>
2019-06-14 08:54:15 +0200

    Updated home page (w/pdf)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h3 class="m-auto">Søk ditt regnr her:</h3>
-            <form class="form-inline">
-                <i class="fas fa-search" aria-hidden="true"></i>
-                <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="AA 12345" aria-label="AA 12345">
-            </form>
-        </div>
-    </div>
+<div class="jumbotron">
+	<h1>Lei av folk som parkerer som en <em><a href="{{ url('/om') }}">idiot</a></em>?</h1>
+	<p class="lead">Du er ikke alene, det er mange der ute som irriterer seg over slikt. Her kan du finne haugevis med folk som har parkert som en <em><a href="{{ url('/om') }}">idiot</a></em>.</p>
+	<p class="lead"><em>Kanskje</em> du er en av dem? Trykk på knappen nedenfor og se om du finner en av dine biler. <em>Kanskje</em> du ønsker å vise folket enda en idiot? Registrer deg og last opp bilde og litt info om feilparkeringen.</p>
+	<p class="lead">Ønsker du å vise <em><a href="{{ url('/om') }}">idioten</a></em> at de har parkert feil? Da kan du laste ned PDF-en for å printe ut og henge på ruta.</p>
+	<p class="lead">
+		<a href="{{ asset('pdf/duparkerersomenidiot_no.pdf') }}" class="btn btn-success"><i class="fas fa-download"></i> Last ned PDF</a>
+		<small><em> ~ eller ~ </em></small>
+		<a href="{{ route('parking.index') }}" class="btn btn-info"><i class="fas fa-car"></i> Se feilparkerte biler</a>
+	</p>
 </div>
 @endsection
