ef0ae8c8bc89b180f5f163117fec947fe0ab3fe0
Daniel Billing <daniel@infihex.com>
2019-07-18 20:32:05 +0200

    Change sorting of submissions

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -30,7 +30,7 @@ class SubmissionController extends Controller
      */
     public function index()
     {
-        return view('submission.index')->withSubmissions(Submission::orderBy('parked_at', 'desc')->paginate(20));
+        return view('submission.index')->withSubmissions(Submission::orderBy('created_at', 'desc')->paginate(20));
     }
 
     /**
