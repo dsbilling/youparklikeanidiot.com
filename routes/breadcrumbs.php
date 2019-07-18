9468851da67db4af6f655a7bf830d56d24688ceb
Daniel Billing <daniel@infihex.com>
2019-07-18 20:33:04 +0200

    Changed language for home

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,7 +2,7 @@
 
 // Home
 Breadcrumbs::for('home', function ($trail) {
-     $trail->push('Home', route('home'));
+     $trail->push('Hjem', route('home'));
 });
 
 Breadcrumbs::for('login', function ($trail) {
