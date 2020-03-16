09534db92c43d25ca12501d24e01539294c76440
Daniel Billing <daniel@infihex.com>
2020-03-16 14:47:45 +0100

    Added support for different urls in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
         <nav class="navbar navbar-expand-md navbar-dark bg-primary border-bottom shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name') }}
+                    {{ request()->getHttpHost() }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
