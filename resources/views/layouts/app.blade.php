f787a25e745d2578d64c66d0b18ac322c898d454
Daniel Billing <daniel@infihex.com>
2020-03-16 14:47:28 +0100

    Added custom css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
     @yield('css')
 </head>
