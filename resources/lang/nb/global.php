d4bbdd61e2be683cd7cee05efaf0ed5d18489191
Daniel Billing <daniel@infihex.com>
2020-03-16 21:44:00 +0100

    Added norwegian translation for DPSEI-10

---
+++ resources/lang/nb/global.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'home' => 'Hjem',
+    'search' => 'Søk',
+    'submit' => 'Send inn',
+    'sendfeedback' => 'Send Feedback',
+    'developedwith' => 'Utviklet med <span class="text-danger">&#10084;</span> av',
+
+    'name' => 'Navn',
+    'username' => 'Brukernavn',
+    'email' => 'E-post',
+    'password' => 'Passord',
+    'confirm' => 'Bekreft',
+    'sendemail' => 'Send E-post',
+
+];
