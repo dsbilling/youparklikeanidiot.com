aa74fe5957f2d413cf54d9ab3e6a0b913cac8a11
Daniel Billing <daniel@infihex.com>
2020-03-17 20:08:48 +0100

    replace periods with underscore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 		{!! __('home.lead', ['url' => url('/info/idiot')]) !!}
 	</div>
 	<p class="mt-5">
-		<a href="{{ asset('pdf/'.strtolower(app()->request->getHost()).'.pdf') }}" class="btn btn-success"><i class="fas fa-download"></i> {{ __('home.downloadpdf') }}</a>
+		<a href="{{ asset('pdf/'.str_replace('.', '_', strtolower(app()->request->getHost()).'.pdf')) }}" class="btn btn-success"><i class="fas fa-download"></i> {{ __('home.downloadpdf') }}</a>
 		<small class="mx-2"><em> ~ {{ __('global.or') }} ~ </em></small>
 		<a href="{{ route('parking.index') }}" class="btn btn-info"><i class="fas fa-parking"></i> {{ __('home.seeparkings') }}</a>
 	</p>
