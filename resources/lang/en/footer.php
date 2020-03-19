b71eb0113a5522af09043c5ebd53838cb487adb0
Daniel Billing <daniel@infihex.com>
2020-03-19 21:57:44 +0100

    Added missing resetdbandsettings trans DPSEI-10

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -3,6 +3,7 @@
 return [
 
     'debugmode' => 'DEBUGMODE',
+    'resetdbandsettings' => 'RESET DB AND SETTINGS',
     'sendfeedback' => 'Send Feedback',
     'developedwith' => 'Developed with <span class="text-danger">&#10084;</span> by',
 
--- resources/lang/nb/footer.php
+++ resources/lang/nb/footer.php
@@ -3,6 +3,7 @@
 return [
 
     'debugmode' => 'DEBUGMODE',
+    'resetdbandsettings' => 'RESET DB AND SETTINGS',
     'sendfeedback' => 'Send tilbakemelding',
     'developedwith' => 'Utviklet med <span class="text-danger">&#10084;</span> av',
 
