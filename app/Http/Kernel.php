c4577352db77d9882f8b285b78d913cd87334c3a
Daniel Billing <daniel@infihex.com>
2020-04-11 10:34:25 +0200

    Commented out domainlocalize, cus it is unused

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \DPSEI\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            //\DPSEI\Http\Middleware\DomainLanguageMiddleware::class,
         ],
 
         'api' => [
@@ -63,7 +62,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'setTheme' => \Igaster\LaravelTheme\Middleware\setTheme::class,
-        //'domainlocalize' => \DPSEI\Http\Middleware\DomainLanguageMiddleware::class,
     ];
 
     /**
