da9cdb926eb03aec9e0ada2ff8a50af91535700f
Daniel Billing <daniel@infihex.com>
2020-08-20 22:12:11 +0200

    DPSEI-23

--- resources/views/layouts/changelog.blade.php
+++ resources/views/layouts/changelog.blade.php
@@ -1,27 +1,19 @@
 
 <h1>0.1.2 - 17. April 2020</h1>
-<h2>        Bug
-</h2>
+<h2>Bug</h2>
 <ul>
-<li>[<a href='http://jira.infihex.com:8080/browse/DPSEI-18'>DPSEI-18</a>] -         Missing parking types
-</li>
-<li>[<a href='http://jira.infihex.com:8080/browse/DPSEI-19'>DPSEI-19</a>] -         Uploading images does not work
-</li>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-18'>DPSEI-18</a>] - Missing parking types</li>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-19'>DPSEI-19</a>] - Uploading images does not work</li>
 </ul>
                 
 
 <h1>0.1.1 - 16. April 2020</h1>
-<h2>        New Feature
-</h2>
+<h2>New Feature</h2>
 <ul>
-<li>[<a href='http://jira.infihex.com:8080/browse/DPSEI-14'>DPSEI-14</a>] -         Let users anon their username and name
-</li>
-<li>[<a href='http://jira.infihex.com:8080/browse/DPSEI-15'>DPSEI-15</a>] -         Add changelog to website
-</li>
-<li>[<a href='http://jira.infihex.com:8080/browse/DPSEI-16'>DPSEI-16</a>] -         User: Change password
-</li>
-<li>[<a href='http://jira.infihex.com:8080/browse/DPSEI-17'>DPSEI-17</a>] -         User: Edit profile
-</li>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-14'>DPSEI-14</a>] - Let users anon their username and name</li>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-15'>DPSEI-15</a>] - Add changelog to website</li>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-16'>DPSEI-16</a>] - User: Change password</li>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-17'>DPSEI-17</a>] - User: Edit profile</li>
 </ul>
 
 <h1>0.1.0 - 11. April 2020</h1>
