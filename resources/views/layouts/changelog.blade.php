358fb4136171c14f3b9e2aec707b3c6c03ef2048
Daniel Billing <daniel@infihex.com>
2020-08-20 22:16:39 +0200

    Updated changelog

--- resources/views/layouts/changelog.blade.php
+++ resources/views/layouts/changelog.blade.php
@@ -1,11 +1,26 @@
 
+<h1>0.1.3 - 20. August 2020</h1>
+<h2>New Feature</h2>
+<ul>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-20'>DPSEI-20</a>] - Add user list</li>
+</ul>
+<h2>Task</h2>
+<ul>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-23'>DPSEI-23</a>] - Update changelog urls</li>
+</ul>
+<h2>Improvement
+</h2>
+<ul>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-21'>DPSEI-21</a>] - Improve design for mobile</li>
+</ul>
+
+
 <h1>0.1.2 - 17. April 2020</h1>
 <h2>Bug</h2>
 <ul>
     <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-18'>DPSEI-18</a>] - Missing parking types</li>
     <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-19'>DPSEI-19</a>] - Uploading images does not work</li>
 </ul>
-                
 
 <h1>0.1.1 - 16. April 2020</h1>
 <h2>New Feature</h2>
