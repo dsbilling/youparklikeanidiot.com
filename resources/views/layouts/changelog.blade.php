40d569beaa6e59c45fbe94a7a66ff877bff718ee
Daniel Billing <daniel@infihex.com>
2020-09-02 22:57:28 +0200

    Updated chanelog

--- resources/views/layouts/changelog.blade.php
+++ resources/views/layouts/changelog.blade.php
@@ -1,4 +1,10 @@
 
+<h1>0.1.4 - 2. September 2020</h1>
+<h2>Bug</h2>
+<ul>
+    <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-24'>DPSEI-24</a>] - InvalidSearchableModel search.show error</li>
+</ul>
+
 <h1>0.1.3 - 20. August 2020</h1>
 <h2>New Feature</h2>
 <ul>
@@ -13,7 +19,6 @@
 <ul>
     <li>[<a href='https://infihex.atlassian.net/browse/DPSEI-21'>DPSEI-21</a>] - Improve design for mobile</li>
 </ul>
-
 
 <h1>0.1.2 - 17. April 2020</h1>
 <h2>Bug</h2>
