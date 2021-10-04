fe999ab41e51795a18217bc2644fa8e37ea27c83
Daniel S. Billing <daniel.billing@intility.no>
2021-10-04 18:10:39 +0200

    Goodbye.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -38,9 +38,21 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">503</div>
-				<div class="hero">{{ trans('global.maintenance') }}</div>
+				<div class="title">Project has been archived!</div>
+				<div class="hero">Thanks to all visitors and users of this project.</div>
+				<p style="max-width: 24em; margin:1em auto;">This was a project intended for me to learn more about Laravel, but I have not had the time to maintain this anymore. The traffic has also been lower than it used to be, so I cannot see any reason for it to live anymore.<br> &mdash; <a href="https://daniel.rtrd.no/">Daniel S. Billing</a></p>
+				{{--
+					<div class="title">503</div>
+					<div class="hero">{{ trans('global.maintenance') }}</div>
+				--}}
 			</div>
 		</div>
+		<script type="text/javascript">   
+			function Redirect() 
+			{  
+				window.location = "https://www.infihex.no"; 
+			}
+			setTimeout('Redirect()', 10000);   
+		</script>
 	</body>
 </html>
